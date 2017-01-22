7cee20ee0db8f958d42f94936e5c8ccdf3ca19f9
Faiz Authar <faizauthar@gmail.com>
2017-01-22 17:21:19 +0700

    omni-ify
    Signed-off-by: Faiz Authar <faizauthar@gmail.com>

--- omni_ferrari.mk
+++ omni_ferrari.mk
@@ -1,5 +1,6 @@
 # Copyright (C) 2014 The CyanogenMod Project
 #           (C) 2017 The LineageOS Project
+#           (C) 2017 Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,17 +14,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# must be before including omni part
+TARGET_BOOTANIMATION_SIZE := 640x362
+
 $(call inherit-product, device/xiaomi/ferrari/full_ferrari.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ferrari
-PRODUCT_NAME := lineage_ferrari
+PRODUCT_NAME := omni_ferrari
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 4i
 PRODUCT_MANUFACTURER := Xiaomi
