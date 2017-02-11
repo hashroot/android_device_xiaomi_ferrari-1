9410fe7a3d0396fc04c817930fb0b26eb789abd6
thewisenerd <thewisenerd@protonmail.com>
2017-02-11 05:36:35 +0530

    light: finalize module; add on/off MS

--- liblight/lights.c
+++ liblight/lights.c
@@ -53,16 +53,16 @@ const char* const LED_FILE[] = {
 	"/sys/class/leds/blue/brightness",
 };
 
-const char* const LED_BLINK[] = {
-	"/sys/class/leds/red/blink",
-	"/sys/class/leds/green/blink",
-	"/sys/class/leds/blue/blink",
-};
-
 const char* const LED_RISE[] = {
 	"/sys/class/leds/red/risetime",
 	"/sys/class/leds/green/risetime",
 	"/sys/class/leds/blue/risetime",
+};
+
+const char* const LED_HIGH[] = {
+	"/sys/class/leds/red/delay_on",
+	"/sys/class/leds/green/delay_on",
+	"/sys/class/leds/blue/delay_on",
 };
 
 const char* const LED_FALL[] = {
@@ -71,8 +71,34 @@ const char* const LED_FALL[] = {
 	"/sys/class/leds/blue/falltime",
 };
 
+const char* const LED_LOW[] = {
+	"/sys/class/leds/red/delay_off",
+	"/sys/class/leds/green/delay_off",
+	"/sys/class/leds/blue/delay_off",
+};
+
 const size_t LED_SIZE
 	= sizeof(LED_FILE) / sizeof(LED_FILE[0]);
+
+const int rf_times_ms[] = {
+	   2,  262,  524,  1049,
+	2097, 4194, 8389, 16780
+};
+
+const size_t rf_times_size
+	= sizeof(rf_times_ms) / sizeof(rf_times_ms[0]);
+
+inline size_t get_closest_rf(int ms) {
+	int curr = 0;
+	size_t i = 0;
+
+	for (i = 0; i < rf_times_size; i++) {
+		if (abs(ms - rf_times_ms[i]) < abs(ms - rf_times_ms[curr]))
+			curr = i;
+	}
+
+	return curr;
+}
 
 /**
  * device methods
@@ -99,7 +125,7 @@ write_int(char const* path, int value)
 		return amt == -1 ? -errno : 0;
 	} else {
 		if (already_warned == 0) {
-			ALOGE("write_int failed to open %s\n", path);
+			ALOGE("write_int failed[%d] to open %s\n", fd, path);
 			already_warned = 1;
 		}
 		return -errno;
@@ -118,17 +144,48 @@ rgb_to_brightness(const struct light_state_t *state)
 static int
 is_lit(struct light_state_t const* state)
 {
-	return state->color & 0xffffffff;
+	return state->color & 0x00ffffff;
 }
 
-void reset_leds(void) {
+inline void reset_leds(void) {
 	size_t i;
 
 	for (i = 0; i < LED_SIZE; i++) {
-		write_int(LED_FILE[i], 0);	// switch off
-		write_int(LED_BLINK[i], 0);	// no blink
-		write_int(LED_RISE[i], 3);	// 1s rise
-		write_int(LED_FALL[i], 3);	// 1s fall
+		write_int(LED_FILE[i], 0);	// reset state
+		write_int(LED_RISE[i], 0);	// reset rise
+		write_int(LED_FALL[i], 0);	// reset fall
+	}
+}
+
+void write_delay(
+	const char* const rf_path[],
+	const char* const delay_path[],
+	int ms
+) {
+	size_t i;
+	int tMS;
+
+	tMS = get_closest_rf(ms);		// compute r/f
+	for (i = 0; i < LED_SIZE; i++) {
+		ALOGD("%s: writing r/f=%d to %s\n", __func__, tMS, rf_path[i]);
+		write_int(rf_path[i], tMS);	// write r/f to sysfs
+	}
+
+	// balance correction
+	tMS = ms - rf_times_ms[tMS];
+
+	ALOGD("%s: balance correction = %d\n", __func__, tMS);
+
+	if (tMS < 0) {
+		for (i = 0; i < LED_SIZE; i++) {
+			ALOGD("%s: writing r/f=0 to %s\n", __func__, delay_path[i]);
+			write_int(delay_path[i], 0);
+		}
+	} else {
+		for (i = 0; i < LED_SIZE; i++) {
+			ALOGD("%s: writing r/f=%d to %s\n", __func__, tMS, delay_path[i]);
+			write_int(delay_path[i], tMS);
+		}
 	}
 }
 
@@ -142,21 +199,11 @@ set_speaker_light_locked(struct light_device_t *dev,
 	int onMS, offMS;
 	size_t i;
 
-	if (!dev) {
-		reset_leds();
-		return -1;
-	}
-
-	if (state == NULL) {
-		return 0;
-	}
-
 	colorRGB = state->color;
-	alpha = (colorRGB >> 24) & 0xFF;
 
-	rgb[0] = ((colorRGB >> 16) & 0xFF) * alpha / 255;	// red
-	rgb[1] = ((colorRGB >> 8) & 0xFF) * alpha / 255;	// green
-	rgb[2] = (colorRGB & 0xFF) * alpha / 255;		// blue
+	rgb[0] = (colorRGB >> 16) & 0xFF;	// red
+	rgb[1] = (colorRGB >> 8) & 0xFF;	// green
+	rgb[2] = colorRGB & 0xFF;		// blue
 
 	switch (state->flashMode) {
 	case LIGHT_FLASH_TIMED:
@@ -171,6 +218,12 @@ set_speaker_light_locked(struct light_device_t *dev,
 		break;
 	}
 
+	ALOGD("%s: onMS = %d; offMS = %d;\n", __func__, onMS, offMS);
+
+	// reset LED state
+	reset_leds();
+
+	// write RGB
 	for (i = 0; i < LED_SIZE; i++) {
 		write_int(LED_FILE[i], rgb[i]);
 	}
@@ -185,6 +238,7 @@ set_speaker_light_locked(struct light_device_t *dev,
 	 *
 	 * kernel supports the following rise/fall times:
 	 * 2048us, 262ms, 524ms, 1.049s, 2.097s, 4.194s, 8.389s, 16.78s
+	 *    0  ;   1  ;   2  ;   3   ;    4  ;    5  ;   6   ;   7
 	 *
 	 * tryin to ease-in-out, let try to use the following values;
 	 * 		rise		thigh		fall		tlow
@@ -223,7 +277,8 @@ set_speaker_light_locked(struct light_device_t *dev,
 	if (onMS == 0 || onMS == 1) { // LED always ON
 		;
 	} else { // LED blink
-		;
+		write_delay(LED_RISE, LED_HIGH, onMS);
+		write_delay(LED_FALL, LED_LOW, offMS);
 	}
 
 	return 0;
@@ -232,6 +287,10 @@ set_speaker_light_locked(struct light_device_t *dev,
 static void
 handle_speaker_light_locked(struct light_device_t *dev)
 {
+	ALOGD("%s: g_attention->color: %x\n", __func__, g_attention.color);
+	ALOGD("%s: g_notification->color: %x\n", __func__, g_notification.color);
+	ALOGD("%s: g_battery->color: %x\n", __func__, g_battery.color);
+
 	if (is_lit(&g_attention)) {
 		set_speaker_light_locked(dev, &g_attention);
 	} else if (is_lit(&g_notification)) {
@@ -247,6 +306,9 @@ set_light_backlight(struct light_device_t *dev,
 {
 	int err = 0;
 	int brightness = rgb_to_brightness(state);
+
+	ALOGD("%s: color: %x\n", __func__, state->color);
+	ALOGD("%s: brightness: %d\n", __func__, brightness);
 
 	pthread_mutex_lock(&g_lock);
 
@@ -264,6 +326,9 @@ set_light_buttons(struct light_device_t *dev,
 	int err = 0;
 	int brightness = rgb_to_brightness(state);
 
+	ALOGD("%s: color: %x\n", __func__, state->color);
+	ALOGD("%s: brightness: %d\n", __func__, brightness);
+
 	pthread_mutex_lock(&g_lock);
 
 	err = write_int(BUTTONS_FILE, brightness);
@@ -279,7 +344,14 @@ set_light_battery(struct light_device_t *dev,
 {
 	pthread_mutex_lock(&g_lock);
 
+	if (state == NULL) {
+		ALOGE("%s: state == NULL!\n", __func__);
+		return 0;
+	}
+
 	g_battery = *state;
+
+	ALOGD("%s: color: %x\n", __func__, state->color);
 
 	handle_speaker_light_locked(dev);
 
@@ -294,7 +366,14 @@ set_light_notifications(struct light_device_t *dev,
 {
 	pthread_mutex_lock(&g_lock);
 
+	if (state == NULL) {
+		ALOGE("%s: state == NULL!\n", __func__);
+		return 0;
+	}
+
 	g_notification = *state;
+
+	ALOGD("%s: color: %x\n", __func__, state->color);
 
 	handle_speaker_light_locked(dev);
 
@@ -309,7 +388,14 @@ set_light_attention(struct light_device_t *dev,
 {
 	pthread_mutex_lock(&g_lock);
 
+	if (state == NULL) {
+		ALOGE("%s: state == NULL!\n", __func__);
+		return 0;
+	}
+
 	g_attention = *state;
+
+	ALOGD("%s: color: %x\n", __func__, state->color);
 
 	handle_speaker_light_locked(dev);
 
