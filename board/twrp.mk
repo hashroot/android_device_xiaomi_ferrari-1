2003317938db9d744fdaa56fbcf00f91cc0a306b
thewisenerd <thewisenerd@protonmail.com>
2017-02-26 10:50:55 +0530

    add twrp support
    Change-Id: Ibbeadb4cc1ff729761710514f41791e40b5dab97

---
+++ board/twrp.mk
@@ -0,0 +1,20 @@
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 100
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_MTP_DEVICE := "/dev/mtp_usb"
+TW_NO_EXFAT_FUSE := true
+TW_INCLUDE_CRYPTO := false
+BOARD_SUPPRESS_EMMC_WIPE := true
+TW_UNMOUNT_FIRMWARE_ON_BOOT := true
+TWHAVE_SELINUX := true
+TW_USE_TOOLBOX := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
---
+++ product/twrp.mk
@@ -0,0 +1,3 @@
+# TWRP
+PRODUCT_COPY_FILES += \
+    device/xiaomi/ferrari/rootdir/etc/twrp.fstab:recovery/root/etc/twrp.fstab
